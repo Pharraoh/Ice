e0cca0b06e3bb2d6f312c02cf4dc793a4ea924bb
Pharraoh <108560793+Pharraoh@users.noreply.github.com>
2026-01-14 20:00:29 +0100

    smtp

--- ice/settings.py
+++ ice/settings.py
@@ -219,14 +219,28 @@ AUTH_PASSWORD_VALIDATORS = [
 # EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp-relay.brevo.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = '92c9d0001@smtp-brevo.com'
+# EMAIL_HOST_PASSWORD = 'xsmtpsib-ab7b5dd16565e4ffeb8e6c25a04853394cc7fb701698f4b13e2a6bdd79171da8-rA4zsv12K8mfV3Qh'
+# DEFAULT_FROM_EMAIL = 'Link Lovers<linkloversng@gmail.com>'
+
+
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '92c9d0001@smtp-brevo.com'
-EMAIL_HOST_PASSWORD = 'xsmtpsib-ab7b5dd16565e4ffeb8e6c25a04853394cc7fb701698f4b13e2a6bdd79171da8-rA4zsv12K8mfV3Qh'
-DEFAULT_FROM_EMAIL = 'Link Lovers<linkloversng@gmail.com>'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
 
 # EMAIL_BACKEND = "anymail.backends.sendinblue.EmailBackend"
 #
